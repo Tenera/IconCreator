0c9edbbb10b216e0f30b3d953aebaa0a65526834
Peter Van Dender <peter.van.dender@lansweeper.com>
2024-01-29 11:04:54 +0100

    Upgrade to .NET 8

--- IconCreator/MainWindow.xaml.cs
+++ IconCreator/MainWindow.xaml.cs
@@ -1,147 +1,110 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using Microsoft.Win32;
-using Brush = System.Drawing.Brush;
+using Size = System.Drawing.Size;
 
-namespace IconCreator
+namespace IconCreator;
+
+public partial class MainWindow
 {
-    public partial class MainWindow
+    private string _fileName;
+
+    public MainWindow()
     {
-        private string _fileName;
+        InitializeComponent();
+    }
 
-        public MainWindow()
+    private void SelectImage_Click(object sender, RoutedEventArgs e)
+    {
+        try
         {
-            InitializeComponent();
-        }
-
-        private void SelectImage_Click(object sender, RoutedEventArgs e)
-        {
-            try
+            var dialog = new OpenFileDialog
             {
-                var dialog = new OpenFileDialog
-                {
-                    CheckFileExists = true,
-                    Filter = "All image files|*.png;*.jpg;*.jpeg;*.jpe;*.jfif;*.exif;*.bmp;*.tiff;*.tif;*.gif|PNG (*.png)|*.png|JPEG (*.jpg, *.jpeg, *.jpe, *.jfif,*.exif)|*.jpg;*.jpeg;*.jpe;*.jfif;*.exif|BMP (*.bmp)|*.bmp|TIFF (*.tiff, *.tif)|*.tiff;*.tif|GIF (*.gif)|*.gif",
-                    Multiselect = false,
-                    Title = "Select image file"
-                };
-                if (dialog.ShowDialog() == true)
-                {
-                    var image = new BitmapImage(new Uri(dialog.FileName));
-                    ImageBox.Source = image;
-                    _fileName = dialog.FileName;
-                    var fileInfo = new FileInfo(_fileName);
-                    Filename.Text = fileInfo.FullName;
-                    FileSize.Text = BytesToString(fileInfo.Length);
-                    Dimensions.Text = $"{image.PixelWidth} x {image.PixelHeight}";
-                }
-            }
-            catch (Exception ex)
-            {
-                ImageBox.Source = null;
-                _fileName = null;
-                Filename.Clear();
-                FileSize.Clear();
-                Dimensions.Clear();
-                MessageBox.Show(ex.Message, "Error reading image", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-            }
-            //IconFactory.CreateMultiImageIcon("C:\\Users\\peter.van.dender\\Pictures\\Icons\\File.png", "LogViewer.ico");
-            //CreateImage("SplashScreen.scale-200.png", 1240, 600, new SolidBrush(Color.White), (Bitmap)Image.FromFile("C:\\Data\\Git\\Service\\Lansweeper.TestTools.App\\Images\\Lansweeper.png"));
-        }
-
-        private void CreateIcon_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new SaveFileDialog
-            {
-                DefaultExt = ".ico",
-                Filter = "Icon file|*.ico",
-                Title = "Select icon file",
-                OverwritePrompt = true
+                CheckFileExists = true,
+                Filter = "All image files|*.png;*.jpg;*.jpeg;*.jpe;*.jfif;*.exif;*.bmp;*.tiff;*.tif;*.gif|PNG (*.png)|*.png|JPEG (*.jpg, *.jpeg, *.jpe, *.jfif,*.exif)|*.jpg;*.jpeg;*.jpe;*.jfif;*.exif|BMP (*.bmp)|*.bmp|TIFF (*.tiff, *.tif)|*.tiff;*.tif|GIF (*.gif)|*.gif",
+                Multiselect = false,
+                Title = "Select image file"
             };
             if (dialog.ShowDialog() == true)
             {
-                try
-                {
-                    IconFactory.CreateMultiImageIcon(_fileName, dialog.FileName);
-                    MessageBox.Show($"Icon '{dialog.FileName}' was created successfully", "Icon created", MessageBoxButton.OK, MessageBoxImage.Information);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Error creating icon", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-                }
+                var image = new BitmapImage(new Uri(dialog.FileName));
+                ImageBox.Source = image;
+                _fileName = dialog.FileName;
+                var fileInfo = new FileInfo(_fileName);
+                Filename.Text = fileInfo.FullName;
+                FileSize.Text = BytesToString(fileInfo.Length);
+                Dimensions.Text = $"{image.PixelWidth} x {image.PixelHeight}";
+                IconFactory.ValidateSize(new Size(image.PixelWidth, image.PixelHeight));
             }
         }
+        catch (Exception ex)
+        {
+            ImageBox.Source = null;
+            _fileName = null;
+            Filename.Clear();
+            FileSize.Clear();
+            Dimensions.Clear();
+            MessageBox.Show(ex.Message, "Error selecting the image", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        CreateIcon.Visibility = ImageBox.Source == null ? Visibility.Collapsed : Visibility.Visible;
+        CreateFavicon.Visibility = ImageBox.Source == null ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    private void CreateIcon_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new SaveFileDialog
+        {
+            DefaultExt = ".ico",
+            Filter = "Icon file|*.ico",
+            Title = "Select icon file",
+            OverwritePrompt = true
+        };
+        if (dialog.ShowDialog() != true) return;
+
+        try
+        {
+            IconFactory.CreateMultiImageIcon(_fileName, dialog.FileName);
+            MessageBox.Show($"Icon '{dialog.FileName}' was created successfully", "Icon created", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Error creating icon", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+    }
         
-        private void CreateFavicon_Click(object sender, RoutedEventArgs e)
+    private void CreateFavicon_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new SaveFileDialog
         {
-            var dialog = new SaveFileDialog
-            {
-                DefaultExt = ".ico",
-                Filter = "Icon file|*.ico",
-                Title = "Select icon file",
-                OverwritePrompt = true
-            };
-            if (dialog.ShowDialog() == true)
-            {
-                try
-                {
-                    IconFactory.CreateFavIcon(_fileName, dialog.FileName);
-                    MessageBox.Show($"Icon '{dialog.FileName}' was created successfully", "Icon created", MessageBoxButton.OK, MessageBoxImage.Information);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Error creating icon", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-                }
-            }
-        }
+            DefaultExt = ".ico",
+            Filter = "Icon file|*.ico",
+            Title = "Select icon file",
+            OverwritePrompt = true
+        };
+        if (dialog.ShowDialog() != true) return;
 
-        private static string BytesToString(long byteCount)
+        try
         {
-            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
-            if (byteCount == 0) return "0" + suf[0];
-
-            var bytes = Math.Abs(byteCount);
-            var place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
-            var num = Math.Round(bytes / Math.Pow(1024, place), 1);
-            return Math.Sign(byteCount) * num + suf[place];
+            IconFactory.CreateFavIcon(_fileName, dialog.FileName);
+            MessageBox.Show($"Icon '{dialog.FileName}' was created successfully", "Icon created", MessageBoxButton.OK, MessageBoxImage.Information);
         }
-
-        private static void CreateImage(string filename, int width, int height, Brush background, Image icon)
+        catch (Exception ex)
         {
-            var destImage = new Bitmap(width, height);
-
-            destImage.SetResolution(icon.HorizontalResolution, icon.VerticalResolution);
-
-            using (var graphics = Graphics.FromImage(destImage))
-            {
-                graphics.FillRectangle(background, new Rectangle(0, 0, width, height));
-
-                graphics.CompositingMode = CompositingMode.SourceOver;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-                var iconsize = Math.Min(width, height) / 2;
-
-                using (var wrapMode = new ImageAttributes())
-                {
-                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    var destRect = new Rectangle((width - iconsize)/2, (height-iconsize)/2, iconsize, iconsize);
-                    graphics.DrawImage(icon, destRect, 0, 0, icon.Width, icon.Height, GraphicsUnit.Pixel, wrapMode);
-                }
-            }
-
-            using (var s = File.Create(filename))
-            {
-                destImage.Save(s, ImageFormat.Png);
-                s.Close();
-            }
+            MessageBox.Show(ex.Message, "Error creating icon", MessageBoxButton.OK, MessageBoxImage.Exclamation);
         }
+    }
+
+    private static string BytesToString(long byteCount)
+    {
+        string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+        if (byteCount == 0) return "0" + suf[0];
+
+        var bytes = Math.Abs(byteCount);
+        var place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+        var num = Math.Round(bytes / Math.Pow(1024, place), 1);
+        return Math.Sign(byteCount) * num + suf[place];
     }
 }
