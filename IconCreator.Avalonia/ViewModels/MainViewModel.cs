48fa1ce88cfcabfbaf6416207bf2aba95045d632
Peter Van Dender <peter.van.dender@lansweeper.com>
2024-02-19 19:31:16 +0100

    Improvements

--- IconCreator.Avalonia/ViewModels/MainViewModel.cs
+++ IconCreator.Avalonia/ViewModels/MainViewModel.cs
@@ -1,15 +1,14 @@
-﻿using System.IO;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform.Storage;
-using Avalonia.Interactivity;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 
 namespace IconCreator.Avalonia.ViewModels;
 
-public partial class MainViewModel(TopLevel topLevel) : ViewModelBase
+public class MainViewModel(TopLevel topLevel) : ViewModelBase
 {
     private string? _fileName;
     private string? _fileSize;
@@ -84,28 +83,28 @@ public partial class MainViewModel(TopLevel topLevel) : ViewModelBase
             if (files.Count == 1)
             {
                 var file = files[0];
-                var props = await file.GetBasicPropertiesAsync();
                 Image = new Bitmap(file.Path.LocalPath);
+                IconFactory.ValidateSize(Image.Size);
+                var props = await file.GetBasicPropertiesAsync();
                 FileName = file.Path.AbsolutePath;
                 FileSize = BytesToString(props.Size ?? 0);
                 Dimensions = $"{Image.Size.Width} x {Image.Size.Height}";
-                IconFactory.ValidateSize(Image.Size);
             }
         }
         catch (Exception ex)
         {
             Image = null;
-            _fileName = null;
             FileName = null;
             FileSize = null;
             Dimensions = null;
-            //MessageBox.Show(ex.Message, "Error selecting the image", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            var box = MessageBoxManager.GetMessageBoxStandard("Error selecting the image", ex.GetBaseException().Message, ButtonEnum.Ok, Icon.Warning);
+            await box.ShowAsync();
         }
 
         OnPropertyChanged(nameof(HasImage));
     }
 
-    public async Task CreateIcon()
+    public async Task CreateIcon(string type)
     {
         var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
         {
@@ -119,41 +118,26 @@ public partial class MainViewModel(TopLevel topLevel) : ViewModelBase
         {
             try
             {
-                IconFactory.CreateMultiImageIcon(_fileName, file.Path.LocalPath);
+                if (type == "favicon")
+                {
+                    IconFactory.CreateFavIcon(FileName, file.Path.LocalPath);
+                }
+                else
+                {
+                    IconFactory.CreateMultiImageIcon(FileName, file.Path.LocalPath);
+                }
                 
-                //MessageBox.Show($"Icon '{dialog.FileName}' was created successfully", "Icon created", MessageBoxButton.OK, MessageBoxImage.Information);
+                var box = MessageBoxManager.GetMessageBoxStandard("Icon created", $"Icon '{file.Path.LocalPath}' was created successfully", ButtonEnum.Ok, Icon.Info);
+                await box.ShowAsync();
             }
             catch (Exception ex)
             {
-                //MessageBox.Show(ex.Message, "Error creating icon", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                var box = MessageBoxManager.GetMessageBoxStandard("Error creating icon", ex.GetBaseException().Message, ButtonEnum.Ok, Icon.Warning);
+                await box.ShowAsync();
             }
         }
     }
     
-    public async Task CreateFavicon()
-    {
-        var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
-        {
-            Title = "Save Icon File",
-            DefaultExtension =".ico",
-            FileTypeChoices = new FilePickerFileType[] { new("Icon file") { Patterns = ["*.ico"] } },
-            ShowOverwritePrompt = true
-        });
-
-        if (file is not null)
-        {
-            try
-            {
-                IconFactory.CreateFavIcon(_fileName, file.Path.LocalPath);
-                //MessageBox.Show($"Icon '{dialog.FileName}' was created successfully", "Icon created", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-            catch (Exception ex)
-            {
-                //MessageBox.Show(ex.Message, "Error creating icon", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-            }
-        }
-    }
-
     private static string BytesToString(ulong byteCount)
     {
         string[] suf = ["B", "KB", "MB", "GB", "TB", "PB", "EB"]; //Longs run out around EB
