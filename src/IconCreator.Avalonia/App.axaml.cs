1755cac8faf99a1015cfc02d96eb3fff25c45617
Peter Van Dender <peter.van.dender@lansweeper.com>
2026-05-27 13:05:19 +0200

    Upgrade to .NET 10, Avalonia 12, single-project structure
    Move code to src/, merge Desktop into core project, remove WPF
    project, add GitHub Actions release workflow, ignore .claude/.

--- src/IconCreator.Avalonia/App.axaml.cs
+++ src/IconCreator.Avalonia/App.axaml.cs
@@ -1,7 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 
 using IconCreator.Avalonia.ViewModels;
@@ -18,23 +17,12 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        // Line below is needed to remove Avalonia data validation.
-        // Without this line you will get duplicate validations from both Avalonia and CT
-        BindingPlugins.DataValidators.RemoveAt(0);
-
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             var view = new MainWindow();
             desktop.MainWindow = view;
             view.DataContext = new MainViewModel(TopLevel.GetTopLevel(desktop.MainWindow));
         }
-        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
-        {
-            var view = new MainView();
-            singleViewPlatform.MainView = view;
-            view.DataContext = new MainViewModel(TopLevel.GetTopLevel(singleViewPlatform.MainView));
-        }
-
         base.OnFrameworkInitializationCompleted();
     }
 }
